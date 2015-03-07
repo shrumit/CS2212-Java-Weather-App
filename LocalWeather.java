ca1a7b50ca6bb67bb110b2f4d714a6e2ed214242
Jennifer Xin <jennifer.jx.xin@gmail.com>
2015-03-07 16:04:14 -0500

    Update LocalWeather.java

--- LocalWeather.java
+++ LocalWeather.java
@@ -1,6 +1,6 @@
 /**
  * CS2212B - LocalWeather class
- * @author dtsui8
+ * @author dtsui8, jxin6
  */
 
 public class LocalWeather {
@@ -8,8 +8,21 @@ public class LocalWeather {
 	private double time, hours, mins, minTemp, maxTemp, sunriseTime, sunsetTime, windSpeed, windDirection, airPressure, humidity;
 	private int year, month, date;
 	
-	public LocalWeather ( ){
-		
+	public LocalWeather (int year, int month, int date, double time, double hours, double mins, double minTemp, double maxTemp, double sunriseTime, double sunsetTime, double windSpeed, double windDirection, double airPressure, double humidity ){
+		this.year = year;
+		this.month = month;
+		this.date = date;
+		this.time = time;
+		this.hours = hours;
+		this.mins = mins;
+		this.minTemp = minTemp;
+		this.maxTemp = maxTemp;
+		this.sunriseTime = sunriseTime;
+		this.sunsetTime = sunsetTime;
+		this.windSpeed = windSpeed;
+		this.windDirection = windDirection;
+		this.airPressure = airPressure;
+		this.humidity = humidity;
 	}
 	
 	private double getTime( ){
