b902fe2a7955326f3780dddff7709ec353c86ed3
Jennifer Xin <jennifer.jx.xin@gmail.com>
2015-03-07 19:08:48 -0500

    Create AbstractLocalWeather.java

---
+++ AbstractLocalWeather.java
@@ -0,0 +1,52 @@
+public abstract class AbstractLocalWeather{
+
+  private double time, hours, mins, minTemp, maxTemp, sunriseTime, sunsetTime, windSpeed, windDirection, airPressure, humidity;
+  	private int year, month, date;
+  	
+  	public LocalWeather (int year, int month, int date, double time, double hours, double mins, double minTemp, double maxTemp, double sunriseTime, double sunsetTime, double windSpeed, double windDirection, double airPressure, double humidity)
+  	{  		
+  	  this.year = year;  		
+  	  this.month = month;
+  		this.date = date;
+  		this.time = time;
+  		this.hours = hours;
+  		this.mins = mins;
+  		this.minTemp = minTemp;
+  		this.maxTemp = maxTemp;
+  		this.sunriseTime = sunriseTime;
+  		this.sunsetTime = sunsetTime;
+  		this.windSpeed = windSpeed;
+  		this.windDirection = windDirection;
+  		this.airPressure = airPressure;
+  		this.humidity = humidity;
+  	}
+  	
+  	/** gets the current time **/
+  	abstract private double getTime();
+  	
+  	/** gets the local minimum temperature **/
+  	abstract private double getminTemp();
+  	
+  	/** gets the local maximum temperature **/
+  	abstract private double getmaxTemp();
+  	
+  	/** gets the local sunrise time **/
+  	abstract private double getsunriseTime();
+  	
+  	/** gets the local sunset time **/
+  	abstract private double getsunsetTime();
+
+    /** gets the local wind speed **?
+  	abstract private double windSpeed();
+  	
+  	/** gets the local wind direction **?
+  	abstract private double windDirection();
+
+    /** gets the local air pressure **/
+  	abstract private double airPressure();
+  	
+  	/** gets the local humidity **/
+  	abstract private double humidity();
+  	
+  }
+}
