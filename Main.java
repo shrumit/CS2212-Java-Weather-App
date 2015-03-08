fc273bf827aaf68dac2490f26f59adc1d3c27f97
Shrumit Mehta <shrumit.m@hotmail.com>
2015-03-08 10:15:37 -0400

    Upload of Maven Project structure
    restructures entire project
    includes pom.xml

--- Main.java
+++
@@ -1,8 +0,0 @@
-/**
- * CS2212 - Main
- * @author David
- */
-
-public class Main {
-//Will be filled once all other classes are made
-}
---
+++ src/main/java/ca/uwo/csd/cs2212/group5/HelloWorld.java
@@ -0,0 +1,10 @@
+package ca.uwo.csd.cs2212.group5;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("why hello there");
+
+	}
+
+}
